58ea35930910ec17190d1980a6e7fcad4f63c7ec
João Campos <joaon.c.jv@gmail.com>
2024-02-08 11:29:22 -0300

    made the login and sign up buttons responsive

---
+++ frontend/src/components/Button/Mobile.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+import styles from './index.module.css';
+
+const ButtonMobile = ({ title, style }) => {
+
+    const [windowSize, setWindowSize] = useState(window.innerWidth);
+    
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowSize(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleResize)
+
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, [])
+    return ( 
+        <>
+            {windowSize < 768 && (
+                <button className={styles[`${style}`]}>
+                <a href="#">{ title }</a>
+            </button>)}
+            
+        </>
+     );
+}
+ 
+export default ButtonMobile;
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,8 +1,8 @@
-import Button from '../../components/Button';
+import Button from '../../components/Button/index'
+import ButtonMobile from '../../components/Button/Mobile';
 import Navbar from '../../components/Navbar';
 import styles from './index.module.css'
 import imageHome from '../../assets/home.png'
-
 
 const Home = () => {
     return (
@@ -14,13 +14,13 @@ const Home = () => {
                         <img src={imageHome} alt='Home' />
                     </div>
                     <article className={styles.container_content}>
-                        <h1>Organize seus projetos no New Board</h1>
+                        <h1>Organize your projects with New Board</h1>
                         <p>
-                            Colabore, gerencie projetos e alcance novos picos de produtividade. Dos arranha-céus ao home office, a forma como sua equipe trabalha é única - faça tudo isso com de um jeito novo com New Board.
+                            Collaborate, manage projects and reach new peaks in productivity. From skyscrapers to home offices, the way your team works is unique - do it all in a new way with New Board.
                         </p>
                         <div className={styles.container_buttons}>
-                            <Button title='Sign in' style='default' />
-                            <Button title='Login' style='login' />
+                            <Button title='Sign up' style='default' />
+                            <ButtonMobile title='Login' style='login' />
                         </div>
                     </article>
                 </div>
