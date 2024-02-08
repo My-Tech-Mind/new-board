03553c8fa1fb16b6ad43874ee22c757df97f3f95
Samuel Castro <samucacastro2017@gmail.com>
2024-02-08 15:09:06 -0300

    editing constants

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,6 +1,6 @@
 import knex from 'knex';
 const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DATABASE } = process.env;
-export const conn = knex({
+export const connection = knex({
 	  client: 'pg',
 	  connection: {
 	    host : DB_HOST,
