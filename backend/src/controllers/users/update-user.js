5e6aaaa9b949fa34eba045a19e226c6bcff31028
dosilva425 <deboraverzul@gmail.com>
2024-02-09 17:25:17 -0300

    done update user

---
+++ backend/src/controllers/users/update-user.js
@@ -0,0 +1,27 @@
+import { connection as knex } from '../../database/connection.js';
+import bcrypt from 'bcrypt';
+
+const updateUser = async (req, res) => {
+    const {name, email, password} = req.body;
+
+    try{
+        const emailValidationUpdate = await knex('users').select('email').where({email}).andWhereNot({id: req.user.id}).first();
+    if (emailValidationUpdate) {
+        return res.status(400).json({message: 'This email is already registered.'});
+    }
+
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    const updateUser = await knex('users').update({
+        name,
+        email,
+        password: encryptedPassword
+    }).where({id: req.user.id}).returning(['id', 'name', 'email']);
+
+    return res.status(200).json(updateUser[0]);
+    } catch (error) {
+        return res.status(500).json({message: 'Internal server error'})
+    }
+};
+
+export { updateUser };
--- backend/src/routers.js
+++ backend/src/routers.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import { updateUser } from './controllers/users/update-user.js';
 
 const router = express.Router(); 
 
 router.get('/', async (req, res)=>{
 	return res.status(200).json({status: "Ok"});
 });
+
+router.put('/user', updateUser);
 
 export { router };
