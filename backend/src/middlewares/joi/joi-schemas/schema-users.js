bfbe59f0cb262071542bfca35a6ba962813588b1
Bonates <natanbonatescursos@hotmail.com>
2024-02-10 13:16:22 -0300

    Update language to English

--- backend/src/middlewares/joi/joi-schemas/schema-users.js
+++ backend/src/middlewares/joi/joi-schemas/schema-users.js
@@ -1,20 +1,20 @@
 import { joi } from 'joi';
 
 const bodyCreateUser = joi.object({
-    nome: joi.string().required().messages({
-        'any.required': 'O campo nome é obrigatório.',
-        'string.empty': 'O campo nome não pode ser vazio.'
+    name: joi.string().required().messages({
+        'any.required': 'The name field is required.',
+        'string.empty': 'The name field cannot be empty.'
     }),
     email: joi.string().email().required().messages({
-        'any.required': 'O campo email é obrigatório.',
-        'string.empty': 'O campo email não pode ser vazio.',
-        'string.email': 'O campo email deve ser um email válido.'
+        'any.required': 'The email field is required.',
+        'string.empty': 'The email field cannot be empty.',
+        'string.email': 'The email field must be a valid email address.'
     }),
-    senha: joi.string().min(8).max(22).required().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]+$/).messages({
-        'any.required': 'O campo senha é obrigatório.',
-        'string.empty': 'O campo senha não pode ser vazio.',
-        'string.pattern.base': 'A senha deve ter pelo menos 8 caracteres, uma letra maiúscula, um número e um símbolo.'
+    password: joi.string().min(8).max(22).required().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]+$/).messages({
+        'any.required': 'The password field is required.',
+        'string.empty': 'The password field cannot be empty.',
+        'string.pattern.base': 'The password must have at least 8 characters, one uppercase letter, one digit, and one special character.'
     })
 });
 
-export {bodyCreateUser}
+export { bodyCreateUser };
