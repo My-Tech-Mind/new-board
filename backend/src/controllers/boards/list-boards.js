389096403fcf3c6c5923944771e719e9a8bb3763
Samuel Castro <samucacastro2017@gmail.com>
2024-02-11 15:40:20 -0300

    listagem boards favoritos

--- backend/src/controllers/boards/list-boards.js
+++ backend/src/controllers/boards/list-boards.js
@@ -1,9 +1,14 @@
 import { connection as knex } from '../../database/connection.js';
 
 const listBoards = async (req, res)=>{
+	const favorite = req.query.favorite;
 	try{
+		if(favorite){
+			const boards = await knex('boards').where('favorited', true);
+			return res.status(200).json(boards);
+		}
 		const boards = await knex('boards');
-		return res.status(200).json(boards)
+		return res.status(200).json(boards);
 	}catch(error){
 		return res.status(500).json({ message: 'Internal server error' });
 	}
