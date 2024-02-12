e58a0d6bc843d3a008451c90daf16c96f8b1a229
Alexia Santos <alexiaosantos17@gmail.com>
2024-02-12 12:28:33 -0300

    third commit - implementation of 'show/hide' password

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -6,13 +6,27 @@ import logoLight from '../../assets/logo-light.png';
 import { FaRegEye } from "react-icons/fa";
 import { MdOutlineMail } from "react-icons/md";
 import { FaRegEyeSlash } from "react-icons/fa";
-
+import { useState } from "react";
 
 const Login = () => {
 
+    const [state, setState] = useState("password");
+    const [iconPasswordStateOne, setIconPassStateOne] = useState("none");
+    const [iconPasswordStateTwo, setIconPassStateTwo] = useState("")
+    const handleClick = () => {
+        if (state == "password") {
+            setState("text");
+            setIconPassStateOne("");
+            setIconPassStateTwo("none");
+        } else if (state == "text") {
+            setState("password");
+            setIconPassStateOne("none");
+            setIconPassStateTwo("");
+        }
+
+    }
 
     return (
-
         <main>
             <div className={styles.container_image}>
                 <img src={ilustracaoLogin} alt='Ilustracao' />
@@ -25,13 +39,13 @@ const Login = () => {
                     <p>Login to the newBoard</p>
                 </div>
                 <div className={styles.input_email}>
-                    <Input placeholder='Insira seu e-mail' style='input_default' />
+                    <Input type='email' placeholder='Insira seu e-mail' style='input_default' />
                     <MdOutlineMail className={styles.icone} />
                 </div>
                 <div className={styles.input_senha}>
-                    <Input type='password' placeholder="Digite a senha" style='input_default' />
-                    <FaRegEye className={styles.icone} />
-                    <FaRegEyeSlash display='none' />
+                    <Input type={state} placeholder="Digite a senha" style='input_default' />
+                    <FaRegEye className={styles.icone} onClick={handleClick} display={iconPasswordStateOne} />
+                    <FaRegEyeSlash className={styles.icone} onClick={handleClick} display={iconPasswordStateTwo} />
                 </div>
                 <div className={styles.container_buttons}>
                     <Button title='Continue' style='default' />
