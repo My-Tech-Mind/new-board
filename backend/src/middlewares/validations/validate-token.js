00eaf0566647cde1827e52bb3a62a56d7f866ec4
Bonates <natanbonatescursos@hotmail.com>
2024-02-12 20:53:41 -0300

    Token bug resolved.

--- backend/src/middlewares/validations/validate-token.js
+++ backend/src/middlewares/validations/validate-token.js
@@ -1,38 +1,48 @@
-import {passwording} from './password-hash.js'
-import  jwt  from 'jsonwebtoken'
-import { connection as knex } from '../../database/connection.js'
-
+import { passwording } from './password-hash.js';
+import jwt from 'jsonwebtoken';
+import { connection as knex } from '../../database/connection.js';
 
 const validateLogin = async (req, res, next) => {
-    const {authorization} = req.headers;
+    const { authorization } = req.headers;
 
-    if(!authorization){
-        return res.status(401).json({message: 'Unauthorized'});
+    
+    if (!authorization) {
+        return res.status(401).json({ message: 'Token not provided' });
     }
 
     try {
+        const token = authorization.replace('Bearer ', ''); 
 
-        const token = authorization.replace('Bearer', '').trim();
+        
+        const decodedToken = jwt.verify(token, passwording); 
 
-        const { id } = jwt.verify(token, passwording);
+        
+        const { id } = decodedToken;
 
-        const userFound = await knex('user').where({id}).first();
+        const userFound = await knex('users').where({ id }).first();
 
-        if(!userFound){
-            return res.status(404).json({message: 'User not found'});
+        if (!userFound) {
+            return res.status(404).json({ message: 'User not found' });
         }
 
-        const {senha, ...user} = userFound;
+        const { senha, ...user } = userFound;
         req.user = user;
 
-
         next();
-
-
     } catch (error) {
-        console.log('Message error:', error.message);
-        res.status(500).json({message: 'Internal server error'});
+
+        if (error instanceof jwt.JsonWebTokenError) {
+            return res.status(401).json({ message: 'Invalid token' });
+
+        } else if (error instanceof jwt.TokenExpiredError) {
+        
+            return res.status(401).json({ message: 'Expired token' });
+        } else {
+            console.log('Message error:', error.message);
+            console.log('ENTROU AQUI');
+            return res.status(500).json({ message: 'Internal server error' });
+        }
     }
 }
 
-export {validateLogin};
+export { validateLogin };
