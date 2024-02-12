8ccb877e118727278c04e90ec1f205b5c5e696ca
Alexia Santos <alexiaosantos17@gmail.com>
2024-02-12 16:21:46 -0300

    fifth commit - language adjustment and responsiveness

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,11 +1,10 @@
 import styles from './index.module.css';
 import Button from '../../components/Button/index';
 import Input from '../../components/Input/index';
-import ilustracaoLogin from '../../assets/ilustracaoLogin.png';
+import ilustrationLogin from '../../assets/ilustrationLogin.png';
 import logoLight from '../../assets/logo-light.png';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import usePasswordToggle from '../../components/Hook/usePasswordToogle';
-
 
 const Login = () => {
     const [PasswordInputType, ToggleIcon] = usePasswordToggle();
@@ -13,7 +12,7 @@ const Login = () => {
         <>
             <main>
                 <div className={styles.container_image}>
-                    <img src={ilustracaoLogin} alt='Ilustracao' />
+                    <img src={ilustrationLogin} alt='Ilustration' />
                 </div>
                 <div className={styles.main_container}>
                     <div className={styles.logo}>
@@ -23,18 +22,18 @@ const Login = () => {
                         <p>Login to the newBoard</p>
                     </div>
                     <div className={styles.input_email}>
-                        <Input type='email' placeholder='Insira seu e-mail' style='input_default' />
+                        <Input type='email' placeholder='Enter your e-mail' style='input_default' />
                         <FontAwesomeIcon icon={['fa', 'envelope']} className={styles.icone} />
                     </div>
                     <div className={styles.input_senha}>
-                        <Input type={PasswordInputType} placeholder="Digite a senha" style='input_default' />
+                        <Input type={PasswordInputType} placeholder="Enter your password" style='input_default' />
                         <span className={styles.icone}>{ToggleIcon}</span>
                     </div>
                     <div className={styles.container_buttons}>
                         <Button title='Continue' style='default' />
                     </div>
                     <div className={styles.container_account}>
-                        <a href="#">Criar uma conta</a>
+                        <a href="#">Create an account</a>
                     </div>
                 </div>
             </main>
