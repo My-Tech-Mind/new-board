e5dedf2c9db94e4ebc27d92455c8f2682f01087a
Bonates <natanbonatescursos@hotmail.com>
2024-02-14 18:34:40 -0300

    Bringing the overall Joi validator into the route.

--- backend/src/routers.js
+++ backend/src/routers.js
@@ -2,7 +2,9 @@ import express from 'express';
 import { updateUser } from './controllers/users/update-user.js';
 import { deleteUser } from './controllers/users/delete-user.js';
 import { createBoard } from './controllers/boards/create-board.js';
-import { validateBodyCreateUser } from './middlewares/joi/joi-validations/validate-body-create-user.js';
+
+import {bodyCreateUser} from './middlewares/joi/joi-schemas/schema-users.js'
+import {validationBodyRequest} from './middlewares/joi/joi-validations/validate-body-request.js'
 import { createUser } from './controllers/users/create-user.js';
 import { validateBodyLogin } from './middlewares/joi/joi-validations/validate-body-login.js';
 
@@ -12,7 +14,7 @@ router.get('/', async (req, res) => {
     return res.status(200).json({ status: "Ok" });
 });
 
-router.post('/user', validateBodyCreateUser, createUser);
+router.post('/user', validationBodyRequest(bodyCreateUser), createUser);
 router.post('/login', validateBodyLogin)
 
 router.put('/user', updateUser);
