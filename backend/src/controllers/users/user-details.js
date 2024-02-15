6489d5b82c7b1e31b0bf8595ceceb7ca9390ea38
Bonates <natanbonatescursos@hotmail.com>
2024-02-15 11:39:17 -0300

    Validation user Details in try catch

--- backend/src/controllers/users/user-details.js
+++ backend/src/controllers/users/user-details.js
@@ -1,24 +1,26 @@
 
 
 const userDetails = async (req, res) => {
-    const {authorization} = req.headers
+    const { authorization } = req.headers
 
     const user = req.user;
-    if (!user) {
-        return res.status(404).json("User not found");
-    }
-    
-    try {
-        
-        const {id, name, email} = user
 
-        return res.status(200).json({id, name, email});
+
+    try {
+
+        const { id, name, email } = user
+
+        if (!user) {
+            return res.status(404).json("User not found");
+        }
+
+        return res.status(200).json({ id, name, email });
     } catch (error) {
 
-        
-        return res.status(500).json({message: error.message});
+
+        return res.status(500).json({ message: error.message });
     }
 
 }
 
-export {userDetails}
+export { userDetails }
