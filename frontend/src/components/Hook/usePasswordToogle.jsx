a1c021dccf4accd61ef23a264df724c19a0222b8
Alexia Santos <alexiaosantos17@gmail.com>
2024-02-14 21:46:48 -0300

    Creation components inputs

--- frontend/src/components/Hook/usePasswordToogle.jsx
+++ frontend/src/components/Hook/usePasswordToogle.jsx
@@ -2,13 +2,13 @@ import React, { useState } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const usePasswordToggle = () => {
-    const [visible, setVisibility] = useState(false);
+    const [visible, setVisibility] = useState(true);
 
     const Icon = (
         <FontAwesomeIcon icon={visible ? "eye-slash" : "eye"}
             onClick={() => setVisibility(visibility => !visibility)} />
     )
-    const InputType = visible ? "text" : "password";
+    const InputType = visible ? "password" : "text";
     return [InputType, Icon]
 }
 
