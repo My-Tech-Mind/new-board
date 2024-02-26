687a085c74d52a51a3a9d3457e35ec1e9c6adcc4
João Campos <joaon.c.jv@gmail.com>
2024-02-26 18:01:11 -0300

    doing the map to appears the cards and tasks

--- frontend/src/pages/Board/index.jsx
+++ frontend/src/pages/Board/index.jsx
@@ -1,10 +1,31 @@
 import Button from '../../components/Button';
 import Header from '../../components/Header';
 import styles from './index.module.css'
+import { useState } from 'react';
 import { FaEllipsisV, FaRegSave, FaPen, FaPlus } from "react-icons/fa";
 
 
 const Board = () => {
+    const initialTasks = [
+        { id: '1', title: 'Task title 1' },
+        { id: '2', title: 'Task title 2' },
+        { id: '3', title: 'Task title 3' }
+    ]
+    const initialCards = [
+        {
+            id: '123',
+            title: 'Card title',
+            tasks: initialTasks
+        },
+        {
+            id: '123',
+            title: 'Card title',
+            tasks: initialTasks
+        }
+    ]
+
+    const [cards, setCards] = useState(initialCards)
+
     const addCard =
         <div className={styles.add_card_container}>
             <FaPlus />
@@ -20,6 +41,7 @@ const Board = () => {
     return ( 
         <>
             <Header logged={true} />
+            
             <div className={styles.main_board}>
                 <div className={styles.board_header}>
                     <div className={styles.board_title_icon}>
@@ -29,50 +51,23 @@ const Board = () => {
                     <Button href='#' title='Save changes' style='board_button'/>
                 </div>
                 <div className={styles.board_container}>
-
+                {initialCards.map((card) => (
                     <div>
                         <div className={styles.card}>
                             <div className={styles.card_title_container}>
-                                <h2 className={styles.card_title}>Card title</h2>
+                                <h2 className={styles.card_title}>{card.title}</h2>
                                 <FaEllipsisV className={styles.title_card_icon} />
                             </div>
-                            <div className={styles.task}>
-                                <h3 className={styles.task_title}>Task title </h3>
+                            {initialTasks.map((task) => (
+                                <div className={styles.task}>
+                                <h3 className={styles.task_title}>{task.title}</h3>
                                 <FaEllipsisV className={styles.icons} />
                             </div>
-                            <div className={styles.task}>
-                                <h3 className={styles.task_title}>Task title </h3>
-                                <FaEllipsisV className={styles.icons} />
-                            </div>
-                            <div className={styles.task}>
-                                <h3 className={styles.task_title}>Task title </h3>
-                                <FaEllipsisV className={styles.icons} />
-                            </div>
+                            ))}
+                            
                         </div>
                     </div>
-
-
-                    <div>
-                        <div className={styles.card}>
-                            <div className={styles.card_title_container}>
-                                <h2 className={styles.card_title}>Card title</h2>
-                                <FaEllipsisV className={styles.title_card_icon} />
-                            </div>
-                            <div className={styles.task}>
-                                <h3 className={styles.task_title}>Task title </h3>
-                                <FaEllipsisV className={styles.icons} />
-                            </div>
-                            <div className={styles.task}>
-                                <h3 className={styles.task_title}>Task title </h3>
-                                <FaEllipsisV className={styles.icons} />
-                            </div>
-                            <div className={styles.task}>
-                                <h3 className={styles.task_title}>Task title </h3>
-                                <FaEllipsisV className={styles.icons} />
-                            </div>
-                        </div>
-                    </div>
-
+                ))}
                     <Button title={addCard} href='#' style='card_button' />
                     
                 </div>
