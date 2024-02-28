0ff52642eb18a1e1d2bad54cc4af5886b43f9cb9
Samuel Castro <samucacastro2017@gmail.com>
2024-02-28 00:06:21 -0300

    removing join and renaming task edit controller

--- backend/src/controllers/tasks/edit-task.js
+++ backend/src/controllers/tasks/edit-task.js
@@ -1,18 +1,14 @@
 import { connection as knex } from '../../database/connection.js';
 import { refreshUpdateDateBoard } from '../../utils/refresh-update-date-board.js';
 
-const updateTask = async (req, res) => {
+const editTask = async (req, res) => {
 	const { title, description, card_id, ordenation } = req.body;
 	const id = req.params.id;
 	try{
-		const taskAndBoardData = await knex('tasks')
-			.join('cards', 'cards.id', '=', 'tasks.card_id')
-			.select('tasks.id', 'board_id')
-			.where('tasks.id', id).first();
-		if (!taskAndBoardData) {
+		const tasks = await knex('tasks').where({id}).first();
+		if (!tasks) {
 			return res.status(404).json({ message: 'Task not found.'});
 		}
-		const { board_id } = taskAndBoardData;
 		const card = await knex('cards').where({ id: card_id }).first();
 		if (!card) {
 			return res.status(404).json({ message: 'Card not found.'});
@@ -23,11 +19,11 @@ const updateTask = async (req, res) => {
 			card_id,
 			ordenation
 		}).where({ id }).returning('*');
-		refreshUpdateDateBoard(board_id);
+		refreshUpdateDateBoard(card.board_id);
 		return res.status(200).json(editedTask[0]);
 	}catch(error){
 		return res.status(500).json({ message: 'Internal server error' });
 	}
 };
 
-export { updateTask };
+export { editTask };
