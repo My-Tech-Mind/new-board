3fd058ae1fa6de0d203a95a5b5fe765429a3e599
João Campos <joaon.c.jv@gmail.com>
2024-02-28 11:15:52 -0300

    made the drag and drop of the tasks without changing positions

--- frontend/src/pages/Board/index.jsx
+++ frontend/src/pages/Board/index.jsx
@@ -3,24 +3,33 @@ import Header from '../../components/Header';
 import styles from './index.module.css'
 import { useState } from 'react';
 import { FaEllipsisV, FaRegSave, FaPen, FaPlus } from "react-icons/fa";
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
 
 const Board = () => {
-    const initialTasks = [
-        { id: '1', title: 'Task title 1' },
-        { id: '2', title: 'Task title 2' },
-        { id: '3', title: 'Task title 3' }
-    ]
+    // const initialTasks = [
+    //     { id: '1', title: 'Task title 1' },
+    //     { id: '2', title: 'Task title 2' },
+    //     { id: '3', title: 'Task title 3' }
+    // ]
     const initialCards = [
         {
             id: '123',
             title: 'Card title',
-            tasks: initialTasks
+            tasks: [
+                { id: '1', title: 'Task title 1' },
+                { id: '2', title: 'Task title 2' },
+                { id: '3', title: 'Task title 3' }
+            ]
         },
         {
-            id: '123',
-            title: 'Card title',
-            tasks: initialTasks
+            id: '321',
+            title: 'Card 2 title',
+            tasks: [
+                { id: '4', title: 'Task title 4' },
+                { id: '5', title: 'Task title 5' },
+                { id: '6', title: 'Task title 6' }
+            ]
         }
     ]
 
@@ -52,25 +61,45 @@ const Board = () => {
 
 
                 <div className={styles.board_container}>
-                {initialCards.map((card) => (
-                    <div>
-                        <div className={styles.card}>
-                            <div className={styles.card_title_container}>
-                                <h2 className={styles.card_title}>{card.title}</h2>
-                                <FaEllipsisV className={styles.title_card_icon} />
-                            </div>
-                            {initialTasks.map((task) => (
-                                <div className={styles.task}>
-                                <h3 className={styles.task_title}>{task.title}</h3>
-                                <FaEllipsisV className={styles.icons} />
-                            </div>
-                            ))}
-                            <Button title={addTask} href='#' style='task_button' />
-                            
-                        </div>
+
+                    <DragDropContext>
+                        {initialCards.map((card) => (
+                        <div>
+                            <Droppable droppableId={card.id}>
+                                {(provided) => (
+                                    <div ref={provided.innerRef} className={styles.card}>
+                                    <div className={styles.card_title_container}>
+                                        <h2 className={styles.card_title}>{card.title}</h2>
+                                        <FaEllipsisV className={styles.title_card_icon} />
+                                    </div>
+                                        {(card.tasks).map((task, index) => (
+                                            <Draggable draggableId={task.id} index={index}>
+                                                {(provided) => (
+                                                    <div
+                                                        {...provided.dragHandleProps}
+                                                        {...provided.draggableProps}
+                                                        ref={provided.innerRef}
+                                                        className={`${styles[{ ...provided.draggableProps.style }]} ${styles.task}`}
+                                                    >
+                                                        <h3 className={styles.task_title}>{task.title}</h3>
+                                                        <FaEllipsisV className={styles.icons} />
+                                                    </div>
+                                                )}
+                                        </Draggable>
+                                        
+                                    ))}
+                                    <Button title={addTask} href='#' style='task_button' />
+                                    
+                                </div>
+                                )}
+                                
+                            </Droppable>
+                        
                     </div>
                 ))}
                     <Button title={addCard} href='#' style='card_button' />
+                    </DragDropContext>
+
                     
                 </div>
             </div>
