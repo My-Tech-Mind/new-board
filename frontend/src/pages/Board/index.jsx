e28327787ca82dcb86065b8a39bc53c5a0de568d
João Campos <joaon.c.jv@gmail.com>
2024-02-29 18:26:00 -0300

    made the drag end drop of the cards

--- frontend/src/pages/Board/index.jsx
+++ frontend/src/pages/Board/index.jsx
@@ -1,10 +1,9 @@
+import { useState } from 'react';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import Button from '../../components/Button';
 import Header from '../../components/Header';
-import styles from './index.module.css'
-import { useState } from 'react';
-import { FaEllipsisV, FaRegSave, FaPen, FaPlus } from "react-icons/fa";
-import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
-
+import { FaEllipsisV, FaPen, FaPlus } from "react-icons/fa";
+import styles from './index.module.css';
 
 const Board = () => {
     const initialCards = [
@@ -35,11 +34,7 @@ const Board = () => {
                 { id: '9', title: 'Task title 9' }
             ]
         }
-    ]
-
-    const [cards, setCards] = useState(initialCards)
-
-    console.log(cards)
+    ];
 
     const addCard =
         <div className={styles.add_card_container}>
@@ -51,114 +46,147 @@ const Board = () => {
         <div className={styles.add_task_container}>
             <FaPlus className={styles.icon_task_plus} />
         </div>
-    
+
+    const [cards, setCards] = useState(initialCards);
+
     const onDragEnd = (result) => {
-        let sourceCardsTasks = []
-        let destinationCardsTasks = []
-        let draggedTask = {}
-        let sourceCardIndex = 0
-        let destinationCardIndex = 0
+        const { destination, source, draggableId, type } = result;
 
-        for (let i in cards) {
-            if (cards[i].id == result.source.droppableId) {
-                sourceCardsTasks = cards[i].tasks
-                sourceCardIndex = i
-            } else if (cards[i].id == result.destination.droppableId) {
-                destinationCardsTasks = cards[i].tasks 
-                destinationCardIndex = i
-            }
-        }
-        console.log(sourceCardsTasks)
-        console.log(destinationCardsTasks)
-
-        for (let i in sourceCardsTasks) {
-            if (sourceCardsTasks[i].id == result.draggableId) {
-                draggedTask = sourceCardsTasks[i]
-            }
+        if (!destination) {
+            return;
         }
 
-        let filteredSourceCardsTasks = sourceCardsTasks.filter((task) => (
-            task.id != result.draggableId))
+        if (type === 'CARD') {
+            const newCards = Array.from(cards);
+            const [movedCard] = newCards.splice(source.index, 1);
+            newCards.splice(destination.index, 0, movedCard);
         
-        if (result.source.droppableId == result.destination.droppableId) {
-            filteredSourceCardsTasks.splice(result.destination.index, 0, draggedTask)
-            
-            let cardsCopy = JSON.parse(JSON.stringify(cards))
-            cardsCopy[sourceCardIndex].tasks = filteredSourceCardsTasks
-            setCards(cardsCopy)
-        } else {
-            destinationCardsTasks.splice(result.destination.index, 0, draggedTask)
-            
-            let cardsCopy = JSON.parse(JSON.stringify(cards))
-            cardsCopy[sourceCardIndex].tasks = filteredSourceCardsTasks
-            cardsCopy[destinationCardIndex].tasks = destinationCardsTasks
-            setCards(cardsCopy)
+            setCards(newCards);
+        } else if (type === 'TASK') {
+            const sourceCard = cards.find((card) => card.id === source.droppableId);
+            const destinationCard = cards.find((card) => card.id === destination.droppableId);
+
+            if (sourceCard === destinationCard) {
+                const newTasks = Array.from(sourceCard.tasks);
+                const [movedTask] = newTasks.splice(source.index, 1);
+                newTasks.splice(destination.index, 0, movedTask);
+
+                const newCard = {
+                    ...sourceCard,
+                    tasks: newTasks,
+                };
+
+                const newCards = cards.map((card) =>
+                    card.id === newCard.id ? newCard : card
+                );
+
+                setCards(newCards);
+            } else {
+                const sourceTasks = Array.from(sourceCard.tasks);
+                const destinationTasks = Array.from(destinationCard.tasks);
+                const [movedTask] = sourceTasks.splice(source.index, 1);
+                destinationTasks.splice(destination.index, 0, movedTask);
+
+                const newSourceCard = {
+                    ...sourceCard,
+                    tasks: sourceTasks,
+                };
+
+                const newDestinationCard = {
+                    ...destinationCard,
+                    tasks: destinationTasks,
+                };
+
+                const newCards = cards.map((card) =>
+                    card.id === sourceCard.id ? newSourceCard : card.id === destinationCard.id ? newDestinationCard : card
+                );
+
+                setCards(newCards);
+            }
         }
-    }
+    };
+
     
-    return ( 
+
+    //<Button title={addCard} href='#' style='card_button' />
+
+    return (
         <>
             <Header logged={true} />
-            
+
             <div className={styles.main_board}>
                 <div className={styles.board_header}>
                     <div className={styles.board_title_icon}>
                         <h1 className={styles.board_title}>Board title</h1>
                         <FaPen className={styles.icons} />
                     </div>
-                    <Button href='#' title='Save changes' style='board_button'/>
+                    <Button href='#' title='Save changes' style='board_button' />
                 </div>
 
                 <div className={styles.board_container}>
-
                     <DragDropContext onDragEnd={onDragEnd}>
-                        {cards.map((card) => (
-                            <div>
-                                <Droppable droppableId={card.id} key={card.id}>
-                                    {(provided) => (
-                                        <div ref={provided.innerRef} className={styles.card}>
-                                        <div className={styles.card_title_container}>
-                                            <h2 className={styles.card_title}>{card.title}</h2>
-                                            <FaEllipsisV className={styles.title_card_icon} />
-                                        </div>
-                                            {(card.tasks).map((task, index) => (
-                                                <Draggable draggableId={task.id} index={index} key={task.id}>
-                                                    {(provided) => (
-                                                        <div
-                                                            {...provided.dragHandleProps}
-                                                            {...provided.draggableProps}
-                                                            ref={provided.innerRef}
-                                                            className={`${styles[{ ...provided.draggableProps.style }]} ${styles.task}`}
-                                                        >
-                                                            <h3 className={styles.task_title}>{task.title}</h3>
-                                                            <FaEllipsisV className={styles.icons} />
+                        <Droppable droppableId="all-cards" direction="horizontal" type='CARD'>
+                                {(provided) => (
+                                    <div
+                                        ref={provided.innerRef}
+                                        {...provided.droppableProps}
+                                        className={styles.board_container}
+                                    >
+                                        {cards.map((card, index) => (
+                                            <div>
+                                                                                                <Draggable draggableId={card.id} index={index} key={card.id}>
+                                                {(provided) => (
+                                                    <div
+                                                        ref={provided.innerRef}
+                                                        {...provided.draggableProps}
+                                                        className={styles.card}
+                                                    >
+                                                        <div className={styles.card_title_container} {...provided.dragHandleProps}>
+                                                            <h2 className={styles.card_title}>{card.title}</h2>
+                                                            <FaEllipsisV className={styles.title_card_icon} />
                                                         </div>
-                                                        
-                                                    )}
-                                                    
+                                                        <Droppable droppableId={card.id} key={card.id} type="TASK">
+                                                            {(provided) => (
+                                                                <div ref={provided.innerRef} {...provided.droppableProps} className={styles.task_container}>
+                                                                    {card.tasks.map((task, index) => (
+                                                                        <Draggable key={task.id} draggableId={task.id} index={index}>
+                                                                            {(provided) => (
+                                                                                <div
+                                                                                    ref={provided.innerRef}
+                                                                                    {...provided.draggableProps}
+                                                                                    {...provided.dragHandleProps}
+                                                                                    className={styles.task}
+                                                                                >
+                                                                                    <h3 className={styles.task_title}>{task.title}</h3>
+                                                                                    <FaEllipsisV className={styles.icons} />
+                                                                                </div>
+                                                                            )}
+                                                                        </Draggable>
+                                                                    ))}
+                                
+                                                                    <div>
+                                                                        <Button title={addTask} href='#' style='task_button' />
+                                                                    </div>
+                                                                    {provided.placeholder}
+                                
+                                                                </div>
+                                                            )}
+                                                        </Droppable>
+                                                    </div>
+                                                )}
                                             </Draggable>
-                                            
+                                            </div>
                                         ))}
                                         {provided.placeholder}
-                                            
-                                    <div>
-                                        <Button title={addTask} href='#' style='task_button' />
                                     </div>
-                                    
-                                </div>
                                 )}
-                                
-                                </Droppable>
-                        
-                        </div>
-                    ))}
-                    <Button title={addCard} href='#' style='card_button' />
+                            
+                        </Droppable>
                     </DragDropContext>
-
                 </div>
             </div>
-        </>
-     );
-}
- 
+</>
+);
+};
+
 export default Board;
