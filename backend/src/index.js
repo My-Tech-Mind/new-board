4a0ef419fa9430c6febf1b028350eee86c1355c5
Samuel Castro <samucacastro2017@gmail.com>
2024-03-23 12:06:39 -0300

    configurando cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { router } from './routers.js';
+import cors from 'cors';
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.use(router);
