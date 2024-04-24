5177b7ae5f64261712f729938245d324ce3ea08f
João Campos <joaon.c.jv@gmail.com>
2024-04-24 10:25:08 -0300

    updating the backend

--- backend/src/controllers/tasks/delete-task.js
+++ backend/src/controllers/tasks/delete-task.js
@@ -8,24 +8,31 @@ const deleteTask = async (req, res) => {
 		const taskAndBoardData = await knex('tasks')
 			.join('cards', 'cards.id', '=', 'tasks.card_id')
 			.join('boards', 'boards.id', '=', 'cards.board_id')
-			.select('board_id', 'boards.user_id as boardOwner', 'tasks.id')
+			.select('board_id', 'boards.user_id as boardOwner', 'tasks.id', 'tasks.ordenation', 'tasks.card_id')
 			.where('tasks.id', id)
 			.first();
 
 		if (!taskAndBoardData) {
 			return res.status(404).json({ message: 'Task not found.' });
 		}
-
+		
 		if (taskAndBoardData.boardOwner != req.user.id) {
 			return res.status(403).json({ message: 'Denied access.' });
 		}
 
-		const { board_id } = taskAndBoardData;
+		const { board_id, ordenation, card_id } = taskAndBoardData;
 		await knex('tasks').where({ id }).delete();
+
+		const limitEditDecrement = ordenation +1;
+		await knex('tasks')
+                    .whereBetween('ordenation', [limitEditDecrement, 20])
+                    .where({ card_id })
+                    .decrement('ordenation', 1);
 		refreshUpdateDateBoard(board_id);
 
 		return res.sendStatus(204);
 	} catch (error) {
+		console.log(error)
 		return res.status(500).json({ message: 'Internal server error' });
 	}
 };
--- backend/src/controllers/tasks/ordenate-tasks.js
+++ backend/src/controllers/tasks/ordenate-tasks.js
@@ -30,6 +30,7 @@ const ordenateTasks = async (req, res) => {
         await knex.transaction(async trx => {
             if (taskSourceDestination < taskSourcePosition) {
                 const limitEditIncrement = Number(taskSourcePosition) - 1;
+                const limitEditDecrement = Number(taskSourcePosition) + 1;
                 await trx('tasks')
                     .where({ id: taskId })
                     .where({ card_id: cardIdDestination })
@@ -44,8 +45,13 @@ const ordenateTasks = async (req, res) => {
                     .where('ordenation', -1)
                     .where({ card_id: cardIdDestination })
                     .update({ ordenation: taskSourceDestination });
+                    
+                await trx('tasks')
+                    .whereBetween('ordenation', [limitEditDecrement, 20])
+                    .where({ card_id: cardIdSource })
+                    .decrement('ordenation', 1);
 
-            } else if (taskSourceDestination > taskSourcePosition) {
+            } else if (taskSourceDestination > taskSourcePosition && cardIdSource === cardIdDestination) {
                 const limitEditDecrement = Number(taskSourcePosition) + 1;
                 await trx('tasks')
                     .where({ id: taskId })
@@ -61,7 +67,52 @@ const ordenateTasks = async (req, res) => {
                     .where({ id: taskId })
                     .where({ card_id: cardIdDestination })
                     .update({ ordenation: taskSourceDestination });
-            }
+
+                await trx('tasks')
+                    .whereBetween('ordenation', [limitEditDecrement, 20])
+                    .where({ card_id: cardIdSource })
+                    .decrement('ordenation', 1);
+
+            } else if (cardIdSource !== cardIdDestination && taskSourceDestination === taskSourcePosition) {
+                const limitEditDecrement = Number(taskSourcePosition) + 1;
+                await trx('tasks')
+                    .where({ id: taskId })
+                    .where({ card_id: cardIdDestination })
+                    .update({ ordenation: -1 });
+
+                await trx('tasks')
+                    .whereBetween('ordenation', [ taskSourcePosition, 20 ])
+                    .where({ card_id: cardIdDestination })
+                    .increment('ordenation', 1);
+
+                await trx('tasks')
+                    .where('ordenation', -1)
+                    .where({ card_id: cardIdDestination })
+                    .update({ ordenation: taskSourceDestination });
+
+                await trx('tasks')
+                    .whereBetween('ordenation', [limitEditDecrement, 20])
+                    .where({ card_id: cardIdSource })
+                    .decrement('ordenation', 1);
+
+        } else if(cardIdSource !== cardIdDestination && taskSourceDestination > taskSourcePosition){
+            const limitEditDecrement = Number(taskSourceDestination);
+
+                await trx('tasks')
+                    .whereBetween('ordenation', [limitEditDecrement, 20])
+                    .where({ card_id: cardIdDestination })
+                    .increment('ordenation', 1);
+
+                await trx('tasks')
+                    .where({ id: taskId })
+                    .where({ card_id: cardIdDestination })
+                    .update({ ordenation: taskSourceDestination });
+
+                await trx('tasks')
+                    .whereBetween('ordenation', [limitEditDecrement, 20])
+                    .where({ card_id: cardIdSource })
+                    .decrement('ordenation', 1);
+        }
 
             refreshUpdateDateBoard(card.board_id);
 
