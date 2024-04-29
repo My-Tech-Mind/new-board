a032eb338f67046e29ef0ea2e1d74f8126156ce8
João Campos <joaon.c.jv@gmail.com>
2024-04-29 10:42:29 -0300

    solving conflicts

--- frontend/src/components/modalComponents/Boards/ModalEditBoard/index.jsx
+++ frontend/src/components/modalComponents/Boards/ModalEditBoard/index.jsx
@@ -1,3 +1,6 @@
+import Button from '../../../Button';
+import Input from '../../../Input';
+import { FaTimes } from 'react-icons/fa';
 import styles from './index.module.css';
 const Modal = ({ Edition, handleInputChange, inputValue }) => {
 
@@ -5,11 +8,18 @@ const Modal = ({ Edition, handleInputChange, inputValue }) => {
         <div className={styles.containerModal}>
             <div className={styles.menu}>
                 <div className={styles.items}>
-                    <p className={styles.titulo}>Boards Title</p>
-                    <input className={styles.input} type="text" value={inputValue} onChange={handleInputChange} />
+                    <h1 className={styles.title}>Create a board</h1>
+                    {/* <input className={styles.input} type="text" value={inputValue} onChange={handleInputChange} /> */}
+                    <FaTimes className={styles.close_icon} onClick={() => Edition('cancel')} />
+                    <div className={styles.input_container}>
+                        <label className={styles.label}>Title board</label>
+                        <Input className={styles.input} defaultValue={inputValue} style='input_default' onChange={handleInputChange} />
+                    </div>
                     <div className={styles.divButton}>
-                        <button className={styles.confirmButton} onClick={() => Edition('finish')}>Ok</button>
-                        <button className={styles.denyButton} onClick={() => Edition('cancel')}>Cancelar</button>
+                        <div onClick={() => Edition('finish')}><Button style='default' title="Create" /></div>
+                        {/* <Button style='negative' title="Cancel" onClick={() => Edition('cancel')}/> */}
+                        {/* <button className={styles.confirmButton} onClick={() => Edition('finish')}>Ok</button>
+                        <button className={styles.denyButton} onClick={() => Edition('cancel')}>Cancelar</button> */}
                     </div>
                 </div>
             </div>
